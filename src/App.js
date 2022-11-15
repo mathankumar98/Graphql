78e90c365c39f67214597b2bd687ffa3b85c0075
Mathan Kumar <104348658+mathankumar98@users.noreply.github.com>
2022-11-15 18:25:21 +0530

    React Graphql Date-15.11.2022

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CharactersList from './pages/CharactersList';
+
+function App() {
+  return(
+     <div className="App">
+    <CharactersList />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from "./reportWebVitals"
+
+import {ApolloClient,ApolloProvider, InMemoryCache}from "@apollo/client";
+
+const client =new ApolloClient({
+  uri:"https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+    <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
+reportWebVitals();
